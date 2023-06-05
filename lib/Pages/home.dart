623fdbe1ec2b1c90f2d52c3196b92987cd934f4f
rk9456rk <62098989+rk9456rk@users.noreply.github.com>
2023-06-05 17:36:43 +0530

    main , pages

---
+++ lib/Pages/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class HomePage extends StatelessWidget {
+  Map<String, double> dataMap = {
+    "Flutter": 5,
+    "React": 3,
+    "Xamarin": 2,
+    "Ionic": 2,
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('PieChart'),
+          centerTitle: true,
+          backgroundColor: Colors.green[700],
+          brightness: Brightness.dark,
+        ),
+        body: Container(
+          child: Center(
+              child: PieChart(
+            dataMap: dataMap,
+            chartRadius: MediaQuery.of(context).size.width / 1.7,
+            legendOptions: LegendOptions(legendPosition: LegendPosition.bottom),
+            chartValuesOptions: ChartValuesOptions(
+              showChartValuesInPercentage: true,
+            ),
+          )),
+        ));
+  }
+}
